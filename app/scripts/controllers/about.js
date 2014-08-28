c4cdac9a405027a22cee380d5b49ca5f6b80ba23
sungwoo <swforgemaster@gmail.com>
2014-08-28 11:03:10 +0900

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ossdbwebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ossdbwebApp
+ */
+angular.module('ossdbwebApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ossdbwebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ossdbwebApp
+ */
+angular.module('ossdbwebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
